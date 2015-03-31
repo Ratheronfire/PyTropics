fc3def56a8a8f29f84a8ba94fb87924a68420c2e
Ratheronfire <Ratheronfire@gmail.com>
2015-03-31 10:55:21 -0400

    Added sprite animation

--- sprite.py
+++ sprite.py
@@ -10,6 +10,8 @@ class Sprite(pygame.sprite.Sprite):
         self.group = pyscroll.PyscrollGroup
         self.map = map
 
+        self.speed = 200
+
         sprites = dict()
         for image in images:
             sprites[image] = pygame.image.load(image)
@@ -18,6 +20,8 @@ class Sprite(pygame.sprite.Sprite):
         self.image = sprites[images[0]]
 
         self.pos = (tile_x, tile_y + 1)
+        self.dx = self.dy = 0
+        self.moving = False
 
         self.rect = self.image.get_rect()
         self.rect.x = tile_x * tile_size
@@ -30,12 +34,28 @@ class Sprite(pygame.sprite.Sprite):
         self.image = self.sprites[image]
 
     def move(self, x, y):
-        if self.map.is_walkable(x, y):
-            self.pos = (x, y)
+        if not self.moving and self.map.is_walkable(x, y):
+            self.dx = x - self.pos[0]
+            self.dy = y - self.pos[1]
+            self.moving = True
 
     def update(self, dt):
-        self.rect.x = self.pos[0] * tile_size
-        self.rect.y = (self.pos[1] - 1) * tile_size
+        self.rect.x += self.dx * self.speed * dt
+        self.rect.y += self.dy * self.speed * dt
+
+        if self.moving and ((self.dx < 0 and self.rect.x < (self.pos[0] - 1) * tile_size) or
+                                (self.dx > 0 and self.rect.x > (self.pos[0] + 1) * tile_size)):
+            self.pos = (self.pos[0] + self.dx, self.pos[1])
+            self.rect.x = self.pos[0] * tile_size
+            self.dx = 0
+            self.moving = False
+
+        if self.moving and ((self.dy < 0 and self.rect.y < (self.pos[1] - 2) * tile_size) or
+                                (self.dy > 0 and self.rect.y > (self.pos[1]) * tile_size)):
+            self.pos = (self.pos[0], self.pos[1] + self.dy)
+            self.rect.y = (self.pos[1] - 1) * tile_size
+            self.dy = 0
+            self.moving = False
 
 class SpriteGroup(pyscroll.PyscrollGroup):
     def __init__(self, map_layer):
@@ -45,6 +65,12 @@ class SpriteGroup(pyscroll.PyscrollGroup):
         self.add(sprite)
 
         sprite.set_group(self)
+
+    def center(self, point):
+        pyscroll.PyscrollGroup.center(self, point)
+
+    def update(self, dt):
+        pyscroll.PyscrollGroup.update(self, dt)
 
 class Player(Sprite):
     def __init__(self, images, tile_x, tile_y, map):
